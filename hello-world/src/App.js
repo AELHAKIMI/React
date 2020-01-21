d3a78fe078c5bdb41509fbf4d9205089b4dd07b6
AELHAKIMI <ayo-voi@hotmail.com>
2020-01-21 09:32:22 +0100

    Home 21/01/2020

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,27 +1,10 @@
 import React from "react";
-//import logo from './logo.svg';
 import "./App.css";
-import RefsDemo from "./components/RefsDemo";
-import FocusInput from "./components/FocusInput";
-// import PureComp from "./PureComp";
-// import ParentComp from "./components/ParentComp";
-// import UserGreeting from "./components/UserGreeting";
-// import Form from "./components/Form";
-// import Table from "./components/Table";
-// import  Greet from './components/Greet'
-// import Message from "./components/Message";
-// import Hello from './components/Hello'
-// import Counter from './components/Counter'
-// import ParentComponent from './components/ParentComponent'
-// import NameList from "./components/NameList";
-// import Stylesheet from './components/Stylesheet'
-// import Inline from './components/Inline'
-// import './appStyles.css'
-// import styles from './appStyles.module.css'
+
 function App() {
   return (
     <div className="App">
-        <FocusInput />
+       
     </div>
   );
 }
